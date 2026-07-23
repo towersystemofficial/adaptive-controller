93a3069a85da9abad86818c5631053db7970277e
towersystemofficial <towersystem.official@gmail.com>
2026-07-22 21:57:35 -0600

    Add documented Lovense BLE transports

--- app/src/main/java/com/towersys/adaptiveremote/device/protocol/Lovense5030ProtocolAdapter.kt
+++ app/src/main/java/com/towersys/adaptiveremote/device/protocol/Lovense5030ProtocolAdapter.kt
@@ -8,6 +8,16 @@ object Lovense5030ProtocolAdapter : BleProtocolAdapter {
     override val supportStatus = AdapterSupportStatus.EXPERIMENTAL
     override val serviceUuid: UUID = UUID.fromString(SERVICE_UUID)
     override val writeCharacteristicUuid: UUID = UUID.fromString(WRITE_CHARACTERISTIC_UUID)
+    override val additionalTransports = setOf(
+        transport("57300001-0023-4bd4-bbd5-a6920e4c5653", "57300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("5a300001-0024-4bd4-bbd5-a6920e4c5653", "5a300002-0024-4bd4-bbd5-a6920e4c5653"),
+        transport("50300001-0023-4bd4-bbd5-a6920e4c5653", "50300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("53300001-0023-4bd4-bbd5-a6920e4c5653", "53300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("5a300001-0023-4bd4-bbd5-a6920e4c5653", "5a300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("4f300001-0023-4bd4-bbd5-a6920e4c5653", "4f300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("42300001-0023-4bd4-bbd5-a6920e4c5653", "42300002-0023-4bd4-bbd5-a6920e4c5653"),
+        transport("43300001-0023-4bd4-bbd5-a6920e4c5653", "43300002-0023-4bd4-bbd5-a6920e4c5653"),
+    )
     override val capabilities = setOf(DeviceCapability.VIBRATION)
 
     override fun encodeScalar(capability: DeviceCapability, value: Int): ByteArray {
@@ -16,6 +26,9 @@ object Lovense5030ProtocolAdapter : BleProtocolAdapter {
     }
 
     override fun encodeStop(): ByteArray = LovenseCommand.stop()
+
+    private fun transport(serviceUuid: String, writeCharacteristicUuid: String) =
+        BleProtocolTransport(UUID.fromString(serviceUuid), UUID.fromString(writeCharacteristicUuid))
 
     const val SERVICE_UUID = "50300001-0024-4bd4-bbd5-a6920e4c5653"
     const val WRITE_CHARACTERISTIC_UUID = "50300002-0024-4bd4-bbd5-a6920e4c5653"
